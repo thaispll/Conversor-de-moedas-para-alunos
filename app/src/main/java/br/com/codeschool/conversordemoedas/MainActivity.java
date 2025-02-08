02ab8b5a39ac51a0e44f155d8e9c2a937aa1e7c5
Thais Leal <thais.pll@hotmail.com>
2025-02-07 21:31:53 -0300

    aula fix 07/02

--- app/src/main/java/br/com/codeschool/conversordemoedas/MainActivity.java
+++ app/src/main/java/br/com/codeschool/conversordemoedas/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editQuantidadeDolar;
@@ -28,10 +31,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void converter(View view){
-        double quantidadeDolar = Double.parseDouble(editQuantidadeDolar.getText().toString());
-        double cotacaoDolar = Double.parseDouble(editCotacaoDolar.getText().toString());
-        double resultado = quantidadeDolar * cotacaoDolar;
+        try {
+            double quantidadeDolar = Double.parseDouble(editQuantidadeDolar.getText().toString());
+            double cotacaoDolar = Double.parseDouble(editCotacaoDolar.getText().toString());
+            double resultado = quantidadeDolar * cotacaoDolar;
 
-        textResultado.setText("Valor convertido em R$: " + resultado);
+            DecimalFormat df = new DecimalFormat("#,##0.00");
+            textResultado.setText("Valor convertido em R$ " + df.format(resultado));
+        } catch (NumberFormatException e){
+            Toast.makeText(this, "Por favor, insira valores válidos!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
